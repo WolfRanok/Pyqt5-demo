bf8c35111ca83755265c2c20e1e881f8e2f87635
WolfRanok <113097087+WolfRanok@users.noreply.github.com>
2022-11-27 15:55:57 +0800

    项目实践

--- QtStudy/项目实践/云函数模拟服务器.py
+++ QtStudy/项目实践/云函数模拟服务器.py
@@ -5,30 +5,53 @@ import json
 """
 
 
-# -*- coding: utf-8 -*-
-
 def handler(event, context):
     # event 参数类型为 dict，event 中包含了触发函数执行的基本信息，可以是平台定义的格式，也可以自定义格式。函数被触发开始执行后，可以在代码内部对 event 进行处理。
     # context 为 SCF 平台提供的入参，将 context 入参传递给执行方法，代码可通过解析 context 入参对象，获取到运行环境及当前请求的相关信息。
     # 封装为特定的数据给百度云一个出口
-    json_params = event.get("body")
+    # 在客户端所提交的文本会以字符串的格式写入
+    json_params = json.loads(event["body"])  # 从这里提取到用户的表单
 
-    user_name = json_params.get("user_name")
+    user_name = json_params["user_name"]
 
-    password = json_params.get("password")
+    password = json_params["password"]
 
-    print("用户名：",user_name)
-    print("密码：",password)
-    val = {"error":"用户名或密码错误"}
+    val = {"error": "用户名或密码错误"}
 
     if user_name == "ranok" and password == "ranok666":
-        val = {"return":"登录成功"}
+        val = {"return": "登录成功"}
 
+    # 发送的格式不会发生改变
+    # 但是浏览器只会显示body的内容,py爬虫则是全部返回
     res = {
         "isBase64Encoded": False,
         "statusCode": 200,
-        "headers": {"X-Custom-Header": "headerValue"},
-        "body": json.dumps(val)
+        "body": val
     }
 
     return res
+
+
+'''
+以下为一个完整的被接接收到的请求参数，其中event的内容如下
+{
+         'resource': '/ranok',
+         'path': '/ranok',
+         'httpMethod': 'POST',
+         'headers': {
+             'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br', 'Connection': 'close',
+             'Content-Length': '46',
+             'Content-Type': 'application/json', 'User-Agent': 'python-requests/2.28.1',
+             'X-Bce-Request-Id': '092ba9bd-7236-45c1-ae7e-ac515d772f1f'
+         },
+         'queryStringParameters': {},
+         'pathParameters': {},
+         'requestContext': {
+             'stage': 'cfc', 'requestId': '092ba9bd-7236-45c1-ae7e-ac515d772f1f',
+             'resourcePath': '/ranok',
+             'httpMethod': 'POST', 'apiId': '1hew6hzryk65t', 'sourceIp': '219.140.59.35'
+         },
+         'body': '{"user_name": "ranok", "password": "ranok666"}', ## 这里是上传上去的信息
+          'isBase64Encoded': False
+}
+'''
--- QtStudy/项目实践/登录模拟.py
+++ QtStudy/项目实践/登录模拟.py
@@ -9,19 +9,24 @@ from MyUi import Ui_Form
 
 class LoginThread(QThread):
     # 在此处创建一个自定义信号
-    start_login_signal = pyqtSignal(str)
+    start_login_signal = None
 
-    def __init__(self):
+    def __init__(self, signal):
         super().__init__()
+        self.start_login_signal = signal
 
-    def login_by_requests(self,user_password_json):
+    def login_by_requests(self, user_password_json):
         # 将字符串对象转换为json格式的对象
         user_password_json = json.loads(user_password_json)
-        print("已发送登录请求",user_password_json)
+        print("已发送登录请求", user_password_json)
         # 这里发送一个requests请求，使用爬虫技术实现
-        resource = requests.post(url=" https://1hew6hzryk65t.cfc-execute.bj.baidubce.com/ranok",json={"body":{"user_name":"ranok","password":"ranok666"}})
-        print("接收到的百度服务器响应：",resource.content.decode())
-
+        # 这里使用json，会使得传入的参数保持json的格式转为字符串例如
+        # 如果是data则会将参数以&串联例如“user_name=ranok&password=ranok666”
+        resource = requests.post(url="https://1hew6hzryk65t.cfc-execute.bj.baidubce.com/ranok",
+                                 json=user_password_json)
+        resource = resource.json()
+        print(resource)
+        self.start_login_signal.emit(json.dumps(resource))
 
     def run(self):
         # 这里是一直在运行的子线程
@@ -31,6 +36,9 @@ class LoginThread(QThread):
 
 
 class MyWindow(QWidget):
+    login_status_signal = pyqtSignal(str)  # 用于接收返回的信号请求
+    login_send_out = pyqtSignal(str)  # 用于发送请求的信号
+
     def __init__(self):
         super().__init__()
         # 利用.ui文件生成的类对象，来创建窗口对象
@@ -41,21 +49,36 @@ class MyWindow(QWidget):
         self.pushButton = self.ui.findChild(QPushButton, 'pushButton')
         self.login_edit = self.ui.findChild(QLineEdit, 'lineEdit')
         self.password_edit = self.ui.findChild(QLineEdit, 'lineEdit_2')
+        self.textBrowser = self.ui.findChild(QTextBrowser, "textBrowser")
+        # self.textBrowser.setText("登陆成功")
 
-        # 新建进程对象
-        self.login_thread = LoginThread()
+        # 新建进程对象，并将信号传入
+        self.login_thread = LoginThread(self.login_status_signal)
+
         # 让线程开始工作
         self.login_thread.start()
 
         self.init_ui()
 
+    def login_status(self, status):
+        """
+        专门用来处理服务器返回请求
+        :param status:
+        :return:
+        """
+        status = json.loads(status)
+        data = status['body']
+        print("接收到的百度服务器响应：", data['return'])
+        self.textBrowser.setText(data['return'])  # 将对应文本显示在textBrowser对象中
+        # self.textBrowser.repaint()  # 刷新界面
+
     def init_ui(self):
         # 绑定按钮点击事件
         self.pushButton.clicked.connect(self.login)
 
-        # 为自定义信号绑定槽函数
-        self.login_thread.start_login_signal.connect(self.login_thread.login_by_requests)
-
+        # 绑定槽函数
+        self.login_send_out.connect(self.login_thread.login_by_requests)  # 处理发送请求的函数
+        self.login_status_signal.connect(self.login_status)  # 处理客户端接收服务器返回的数据函数
 
     def login(self):
         # 获取用户的账号和密码
@@ -63,7 +86,7 @@ class MyWindow(QWidget):
         password = self.password_edit.text()
 
         # 触发信号，并传递参数
-        self.login_thread.start_login_signal.emit(json.dumps({"user_name": user_name, "password": password}))
+        self.login_send_out.emit(json.dumps({"user_name": user_name, "password": password}))
 
         print(user_name, password)
 
